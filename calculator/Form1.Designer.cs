0c3299cfe7628ca44e7ed173c0eb25b41a335d9c
yarvald <yaroslav.pitsukha@gmail.com>
2019-03-28 23:50:16 +0300

    main

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.inform = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(15, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(391, 65);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "область для ввода строки\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(253, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 65);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "очистить поле ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(391, 267);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "операции со строками";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(138, 179);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(109, 64);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "количество слов в строке";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(23, 179);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(109, 64);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "количество предложений";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(253, 109);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(109, 64);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "количество согласных";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(23, 109);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 64);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "длина строки";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(138, 109);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 64);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "количество гласных";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(253, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 64);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "получение символа по индексу";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 64);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "удаление подстроки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 64);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "замена подстроки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // inform
+            // 
+            this.inform.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inform.Location = new System.Drawing.Point(15, 395);
+            this.inform.Name = "inform";
+            this.inform.Size = new System.Drawing.Size(391, 72);
+            this.inform.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "информационное поле";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 476);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inform);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Текстовый калькулятор";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label inform;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ calculator/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+
+        private string text;
+        private string text1;
+        private string text2;
+        private int namber;
+
+        public Form1()
+        {
+            InitializeComponent();
+            namber = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { if(namber == 0)
+            {
+                text = textBox1.Text;
+            }
+            else if(namber == 1)
+            {
+                text1 = textBox1.Text;
+            }
+            else if (namber == 2)
+            {
+                text2 = textBox1.Text;
+            }
+
+            inform.Text = "";
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text = "";
+            textBox1.Text = "";
+            namber = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (namber == 0)
+            {
+                inform.Text = "Введите подстроку которую хотите заменить и нажмите ещё раз";
+                namber++;
+
+            }
+            else if(namber ==1)
+            {
+
+                inform.Text = "Введите подстроку которой хотите заменить и нажмите ещё раз";
+                namber++;
+            }
+            else if(namber ==2)
+            {
+                text = text.Replace(text1, text2);
+                inform.Text = "Выполнено ";
+                textBox1.Text = text;
+                namber = 0;
+                text1 = "";
+                text2 = "";
+               
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (namber == 0)
+            {
+                inform.Text = "Введите подстроку которую хотите удалить и нажмите ещё раз";
+                namber++;
+                
+            }
+            else if (namber == 1)
+            {
+                text = text.Replace(text1, "");
+                inform.Text = "Выполнено " ;
+                textBox1.Text = text;
+                namber = 0;
+                text1 = "";
+              
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (namber == 0)
+                {
+                    inform.Text = "введите номер символа в строке и нажмите ещё раз";
+                    namber++;
+                }
+                else if (namber == 1)
+                {
+                    int i;
+                    i = Convert.ToInt16(text1);
+                    inform.Text = "Выполнено ";
+                    textBox1.Text = text[i + 1].ToString();
+                    text1 = "";
+                    namber = 0;
+                }
+
+            }
+            catch (Exception exept)
+            {
+                inform.Text = exept.Message;
+                inform.ForeColor = Color.Red;
+               
+            }
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+          int q= text.Length;
+            inform.Text = "Выполнено: "+ q;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int d=0;
+            string glas = "ауоыиэяюёеiuАУОЫИЭЯЮЁЕIUY";
+            for (int r = 0; r < text.Length; r++){
+                for(int t = 0; t < glas.Length; t++)
+                {
+                    if (text[r] == glas[t])
+                    {
+                        d++;
+                    }
+                }
+             
+            }
+
+
+            inform.Text = "Выполнено: " +d;
+      
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int d = 0;
+            string glas = "qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNMЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБйцкнгшщзхъфвпрлджчсмтьб";
+            for (int r = 0; r < text.Length; r++)
+            {
+                for (int t = 0; t < glas.Length; t++)
+                {
+                    if (text[r] == glas[t])
+                    {
+                        d++;
+                    }
+                }
+
+            }
+
+
+            inform.Text = "Выполнено: " + d;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int d = 0;
+            for (int r = 0; r < text.Length; r++)
+            {
+               
+                    if (text[r] == '.')
+                    {
+                        d++;
+                    }
+                
+
+            }
+
+
+            inform.Text = "Выполнено: " + d;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int d = 0;
+            for (int r = 0; r < text.Length; r++)
+            {
+                if (text[r] != text[text.Length-1])
+                {
+                    if (text[r] == ' ' && text[r + 1] != ' ')
+                    {
+                        d++;
+                    }
+                }
+               
+             
+
+
+            }
+            if(text[0]!=' ')
+            d++;
+            inform.Text = "Выполнено: " + d;
+        }
+    }
+}
